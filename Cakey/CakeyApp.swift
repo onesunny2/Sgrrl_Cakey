e04cf3d30536b0016cbdadb76f03b67502ff6718
onesunny2 <ippne996@gmail.com>
2024-11-03 21:07:42 +0900

    Initial Commit

---
+++ Cakey/CakeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CakeyApp.swift
+//  Cakey
+//
+//  Created by Lee Wonsun on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CakeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cakey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cakey
+//
+//  Created by Lee Wonsun on 11/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
