721df59b6ba3245540d79e2385c8d9c65753d420
onesunny2 <ippne996@gmail.com>
2024-11-09 05:44:48 +0900

    [feat] #26 ArchieveView navigatoinBackButton 커스텀, ArchieveCell 생성

--- Cakey/Views/OrderFormArchieve/ArchieveView.swift
+++ Cakey/Views/OrderFormArchieve/ArchieveView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct ArchieveView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
     var body: some View {
-        Text("저장된 주문서 목록 화면 입니다.")
+        ZStack {
+            Color.cakeyYellow1
+                .ignoresSafeArea(.all)
+            
+            VStack {
+                Text("나의 케이크 도안")
+                    .customStyledFont(font: .cakeyTitle1, color: .cakeyOrange1)
+                
+                ScrollView {
+                    ArchieveCell()
+                }
+            }
+            .padding(.top, 28)
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Image(systemName: "chevron.backward")
+                        .font(.cakeyCallout)
+                        .foregroundStyle(.cakeyOrange1)
+                }
+            }
+        }
     }
 }
 
