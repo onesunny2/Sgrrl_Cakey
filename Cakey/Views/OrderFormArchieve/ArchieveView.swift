f9a6c60fe4b1764c31e1b7bb7f6dbf4cb0968bbe
onesunny2 <ippne996@gmail.com>
2024-11-09 05:57:48 +0900

    [feat] #26 ArchieveView LazyVGrid 활용해서 UI 구현

--- Cakey/Views/OrderFormArchieve/ArchieveView.swift
+++ Cakey/Views/OrderFormArchieve/ArchieveView.swift
@@ -10,18 +10,28 @@ import SwiftUI
 struct ArchieveView: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
+    var archieveColums: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+    
     var body: some View {
         ZStack {
             Color.cakeyYellow1
                 .ignoresSafeArea(.all)
             
-            VStack {
-                Text("나의 케이크 도안")
-                    .customStyledFont(font: .cakeyTitle1, color: .cakeyOrange1)
+            VStack(spacing: 32) {
+                HStack {
+                    Text("나의 케이크 도안")
+                        .customStyledFont(font: .cakeyTitle1, color: .cakeyOrange1)
+                    Spacer()
+                } .padding(.leading, 20)
                 
                 ScrollView {
-                    ArchieveCell()
+                    LazyVGrid(columns: archieveColums, spacing: 16) {
+                        ForEach(0..<3) { _ in
+                            ArchieveCell()
+                        }
+                    }
                 }
+                .padding(.horizontal, 20)
             }
             .padding(.top, 28)
         }
