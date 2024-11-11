479f0eeacdf44d161280377504671b718ccbcc94
onesunny2 <ippne996@gmail.com>
2024-11-12 00:15:53 +0900

    [feat] #19 Realm 설치 및 manager 생성, 모델 구조 수정 후 viewModel 생성

--- Cakey/Model/CakeyModel.swift
+++ Cakey/Model/CakeyModel.swift
@@ -6,15 +6,63 @@
 //
 
 import Foundation
+import RealmSwift
 
 struct CakeyModel {
+    var id: String = UUID().uuidString
     var cakeColor: String?
     var letteringText: String?
     var letteringColor: String?
     var cakeImages: [decoElements] = []
+    var saveDate: Date = .now
+    var isComplete: Bool = false
 }
 
-struct decoElements {
+struct decoElements: Decodable, Encodable {
     var image: Data
     var description: String
 }
+
+extension CakeyModel: Persistable {
+    typealias PersistedObject = CakeyEntity
+    
+    init(persistedObject: CakeyEntity) {
+        let decoder = JSONDecoder()
+        
+        self.id = persistedObject.id
+        self.cakeColor = persistedObject.cakeColor
+        self.letteringText = persistedObject.letteringText
+        self.letteringColor = persistedObject.letteringColor
+        self.cakeImages = try! decoder.decode([decoElements].self, from: persistedObject.cakeImages!)
+        self.saveDate = persistedObject.saveDate
+        self.isComplete = persistedObject.isCompleted
+    }
+    
+    func persistedObject() -> CakeyEntity {
+        let encoder = JSONEncoder()
+        let cakey = CakeyEntity()
+        
+        cakey.id = self.id
+        cakey.cakeColor = self.cakeColor
+        cakey.letteringText = self.letteringText
+        cakey.letteringColor = self.letteringColor
+        cakey.cakeImages = try! encoder.encode(self.cakeImages)
+        cakey.saveDate = self.saveDate
+        cakey.isCompleted = self.isComplete
+        
+        return cakey
+    }
+}
+
+
+class CakeyEntity: Object {
+    @Persisted(primaryKey: true) var id: String
+    @Persisted var cakeColor: String?
+    @Persisted var letteringText: String?
+    @Persisted var letteringColor: String?
+    @Persisted var cakeImages: Data?
+    @Persisted var saveDate: Date
+    @Persisted var isCompleted: Bool
+}
+
+
